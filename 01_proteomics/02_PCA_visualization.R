ad6483fc6f4e4803908bf3b769434d0a61afc471
songlicheng666 <1937291638@qq.com>
2026-05-07 16:04:06 +0800

    Remove sample definitions for PCA visualization
    Removed sample definitions for AA, AC, CA1, CA2, and CC groups.

--- 01_proteomics/02_PCA_visualization.R
+++ 01_proteomics/02_PCA_visualization.R
@@ -14,11 +14,6 @@ setwd("C:/Users/songl/OneDrive/桌面/多组学分析")# 定义分组样本列�
 # 加载必要包
 library(ggplot2)
 library(ggrepel)
-AA_samples <- c("S28", "S30", "S31", "S32", "S33", "S34", "S35", "S36", "S37")
-AC_samples <- c("S1", "S10", "S11", "S12", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9")
-CA1_samples <- c("S47", "S48", "S49", "S50", "S51", "S52", "S53", "S54", "S55", "S56", "S57")
-CA2_samples <- c("S58", "S59", "S60", "S61", "S62", "S63", "S64", "S65", "S66")
-CC_samples <- c("S15", "S16", "S17", "S18", "S19", "S20", "S21", "S22", "S23", "S24", "S25", "S26", "S27")
 
 # 合并为分组映射列表
 group_mapping <- list(
